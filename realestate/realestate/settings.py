cafeb4ca94700f41ab8ad407d3b69025052511ac
mdzahidsa <118134972+mdzahidsa@users.noreply.github.com>
2023-04-04 01:57:18 +0100

    debug set to false

--- realestate/realestate/settings.py
+++ realestate/realestate/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-31jmbm(cc-9@=lzo5a!g53bw-b%th2lddam!uz=#68ctzd&t@2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
